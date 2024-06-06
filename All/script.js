f47a76ce3dac9ce36405a9d49497bc07eabf7ff9
Dasha Salagaeva <alaa.aloovna@bk.ru>
2024-06-06 15:52:22 +0700

    add all

--- All/script.js
+++ All/script.js
@@ -1,9 +1,7 @@
 const body = document.getElementById('body');
 const slider = document.getElementById('slider');
-
 function dark_mode()
 {
     body.classList.toggle('darkmode');
 }
-
 slider.addEventListener('click', dark_mode);
